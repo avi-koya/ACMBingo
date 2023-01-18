3fcc0d9ebe5bf1ebb43a3785a31fa54b8e990056
Avinash Koya <avinashkoya@hotmail.com>
2023-01-17 19:22:55 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+cards = [
+    "YOOOORSHHH", "stuff it-yorsh", "Knifers?", "pogO", "tatavi clutch", "jaja clutch", "nice try (sarcastically)", "naurrrrr",
+    "i cauuunnttt", "murloc noises", "Knife kill", "avi/nic/yorsh gank", "HEEEHEEEHEEE", "what the josh doin", "bro...",
+    "Psycho", "tofu being a skinfiend", "these guys are ass", "malding-bigdood", "IM DOWN FOR FF", "josh alt tab", "good eco",
+    "ACM push", "tatanny internet gone", "Thanks Queen", "this game is unloseable", "enemy comeback win"
+]
+
+
+
+print(cards)
